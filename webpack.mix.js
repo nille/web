09f776c32798af8fc1bd33403d872aaa489ce074
Nicklas af Ekenstam <ekenstam@amazon.com>
2023-06-05 10:01:02 +0200

    disable mix notifications on success, redux

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,10 @@
 let mix = require('laravel-mix');
 
+mix.disableSuccessNotifications();
+
 mix.copy('src/assets/images', 'dist/images')
     .js('src/assets/js/app.js', 'dist/js')
     .postCss('src/assets/css/app.css', 'dist/css', [
         require('tailwindcss')
     ]
-    .disableSuccessNotifications()
 );
