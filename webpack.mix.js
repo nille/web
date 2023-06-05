e97b0f83f0f96aa6031ee379771cdfe0ccc085cb
Nille af Ekenstam <n@nille.dev>
2023-06-05 22:11:09 +0200

    Various stuff
    * add padding to main content area for sm screens
    
    * hover colors for footer social logos
    
    * testing no change of font thickness in navbar
    
    * testing no change of font thickness in navbar, redux
    
    * disable mix notifications on success
    
    * disable mix notifications on success, redux
    
    * wip
    
    * navbar fix
    
    * update gitignore
    
    * add delete script
    
    * add laravel-mix-simple-image-processing
    
    * add thumbnail generation using mix
    
    * wip: og tags partial
    
    * cleanup of site data

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,18 @@
 let mix = require('laravel-mix');
 
+require('laravel-mix-simple-image-processing')
+
+mix.disableSuccessNotifications();
+
 mix.copy('src/assets/images', 'dist/images')
     .js('src/assets/js/app.js', 'dist/js')
     .postCss('src/assets/css/app.css', 'dist/css', [
         require('tailwindcss')
-    ]
-);
+    ])
+    .imgs({
+        source: 'src/assets/images',
+        destination: 'dist/images',
+        processOriginalImage: true,
+        imageminPngquantOptions: { strip: true, quality: [0.6, 1] },
+        thumbnailsSizes: [300, 600, 1200],
+    });
